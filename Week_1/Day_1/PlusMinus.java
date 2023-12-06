c4aaa73d2e03c2bd3e51231625e78a40885fcf34
Nicolas Martins <nicolas.edm7@gmail.com>
2023-12-05 22:53:16 -0300

    Day 1: Solving Plus Minus

--- Week_1/Day_1/PlusMinus.java
+++ Week_1/Day_1/PlusMinus.java
@@ -2,6 +2,25 @@ package Day_1;
 
     public class PlusMinus {
         public static void main(String[] args) {
-            System.out.println("Hello, world!");
+
+            int size = arr.size();
+            float positives = 0;
+            float negatives = 0;
+            float zeros = 0;
+
+            for(int index = 0 ; index < size ; index ++) {
+                if (arr.get(index) > 0) {
+                    positives ++;
+                } else if (arr.get(index) < 0) {
+                    negatives ++;
+                } else {
+                    zeros ++;
+                }
+            }
+
+            System.out.println(positives/size);
+            System.out.println(negatives/size);
+            System.out.println(zeros/size);
+
         }
     }
